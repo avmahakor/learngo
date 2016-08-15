cdff662a870b7a14d030f03d65b707083fd3e176
Anna Koroleva <send_me_letters@yahoo.com>
2016-08-15 22:14:26 +0000

    First commit

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+    "fmt"
+  "log"
+)
+
+func main(){
+    
+    for i := 0; i < 100; i++ {
+        fmt.Println("hello world no. ",i)
+        
+        // this is some code i'll add so you can see
+        
+        fmt.Println("another line to print out", i)
+    }
+}
+    
